f893ae545e83c167899c7b372db029d7462b2c48
Tashrif <istiaktashrif@gmail.com>
2024-04-29 05:21:54 +0600

    get data for categories

--- index.js
+++ index.js
@@ -56,6 +56,48 @@ async function run() {
       res.send(result); 
     })
 
+    // Clay-Made Pottery
+    app.get('/items/662d5f1eabfe07032c42c4ad', async (req, res) => {
+      const query = {category: "Clay-made pottery"};
+      const cursor = itemsCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result)
+    })
+    // Stoneware
+    app.get('/items/662d6032abfe07032c42c4b1', async (req, res) => {
+      const query = {category: "Stoneware"};
+      const cursor = itemsCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result)
+    })
+    // Porcelain
+    app.get('/items/662d6096abfe07032c42c4b2', async (req, res) => {
+      const query = {category: "Porcelain"};
+      const cursor = itemsCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result)
+    })
+    // Terra Cotta
+    app.get('/items/662d60e3abfe07032c42c4b3', async (req, res) => {
+      const query = {category: "Terra Cotta"};
+      const cursor = itemsCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result)
+    })
+    // Ceramics & Architectural
+    app.get('/items/662d614eabfe07032c42c4b5', async (req, res) => {
+      const query = {category: "Ceramics & Architectural"};
+      const cursor = itemsCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result)
+    })
+    // Home Decor Pottery
+    app.get('/items/662d6209abfe07032c42c4b6', async (req, res) => {
+      const query = {category: "Home decor pottery"};
+      const cursor = itemsCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result)
+    })
 
 
     // Send a ping to confirm a successful connection
